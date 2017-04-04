df0ff784c7ca102d464e81d760e49a11f442d159
Dickyhaha <dickdick1003@yahoo.com.hk>
2017-04-04 22:02:19 +0800

    fusion script ready for running

--- data_fusion/strict_compare.py
+++ data_fusion/strict_compare.py
@@ -12,16 +12,21 @@ entries = cursor.fetchall()
 
 l = len(entries)
 
+count = 0
+
 with open('strict_compare.txt', 'w') as f:
     for i in range(l):
         for j in range(i + 1, l):
             s1 = entries[i][0]
             s2 = entries[j][0]
-            s1 = re.sub('[^\w\s]', '', '' if s1 is None else s1).lower()
-            s2 = re.sub('[^\w\s]', '', '' if s2 is None else s2).lower()
+            s1 = re.sub('[^\w\s]|_', '', '' if s1 is None else s1).lower().strip()
+            s2 = re.sub('[^\w\s]|_', '', '' if s2 is None else s2).lower().strip()
             if s1 == s2:
                 f.write(str(i) + str(j))
                 f.write(entries[i][0])
                 f.write(entries[j][0])
                 f.write('\n')
+                count += 1
+
+    f.write('Total: ' + count)
 
--- data_fusion/string_edit_distance.py
+++ data_fusion/string_edit_distance.py
@@ -16,15 +16,20 @@ temp = []
 
 l = len(entries)
 
+count = 0
+
 with open('string_edit.txt', 'w') as f:
     for i in range(l):
         for j in range(i + 1, l):
             if i != j:
                 s1 = '' if entries[i][0] is None else entries[i][0]
                 s2 = '' if entries[j][0] is None else entries[j][0]
-                a = editdistance.eval(s1, s2) 
-                if a < len(s1) / 3:
+                d = editdistance.eval(s1, s2) 
+                if d < len(s1) / 4:
                     f.write(str(i) + s1)
                     f.write(str(j) + s2)
                     f.write('Edit distance:' +  a)
                     f.write('\n')
+                    count += 1
+
+    f.write('Total: ' + count)
