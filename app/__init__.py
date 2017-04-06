4c4540b33ed8137285f662b57398fc774d024819
budi <budiryan@github.com>
2017-04-07 03:15:06 +0800

    added some bootstraps

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
+Bootstrap(app)
 app.config.from_object('config')
 
 from app import views
--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import Form
-from wtforms import StringField
+from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
 
 
 class QueryForm(Form):
-    search_query = StringField('Search', validators=[DataRequired()])
+    search_query = StringField('Query', validators=[DataRequired()])
+    paper = BooleanField('Papers', default=False)
+    author = BooleanField('Authors', default=False)
